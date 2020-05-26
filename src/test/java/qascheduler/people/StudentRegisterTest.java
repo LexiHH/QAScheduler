3b47906b2f216e2a8306a2efdd361a340af259ac
LexiHH <alexiahh@hotmail.co.uk>
2020-05-26 10:36:58 +0100

    Student register test file created

---
+++ src/test/java/qascheduler/people/StudentRegisterTest.java
@@ -0,0 +1,38 @@
+package qascheduler.people;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentRegisterTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void register() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStream() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createStudentStreamMap() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void putStudentsInStreamMap() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addStudentsToStream() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printStudentStreamEnrollments() {
+    }
+}
